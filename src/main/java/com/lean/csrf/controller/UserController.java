76878bd3cc519122afed239c2539d5120b6bdb18
LeanSaldivar <leanemmanuelsaldivar@gmail.com>
2025-08-16 20:41:35 +0800

    Enable CORS configuration and integrate into security setup

--- src/main/java/com/lean/csrf/controller/UserController.java
+++ src/main/java/com/lean/csrf/controller/UserController.java
@@ -9,10 +9,7 @@ import com.lean.csrf.view.dto.UserRequestDto;
 import com.lean.csrf.view.dto.UserResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v2/users/auth")
