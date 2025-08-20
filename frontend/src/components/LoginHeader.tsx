0255ef1b12a918d032f8aac6c29f5b48cdc25a82
LeanSaldivar <leanemmanuelsaldivar@gmail.com>
2025-08-20 15:31:56 +0800

    Replace App component with Login and add new SCSS styles

---
+++ frontend/src/components/LoginHeader.tsx
@@ -0,0 +1,11 @@
+function LoginHeader() {
+    return (
+        <div className="header">
+           <p>
+                Welcome Back .!
+           </p>
+        </div>
+    )
+}
+
+export default LoginHeader
